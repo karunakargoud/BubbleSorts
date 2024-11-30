a4716f3bb0f04a61ee430501908ba8b42edf2e44
karunakargoud <karunakargoud1116@gmail.com>
2024-11-30 20:36:00 +0530

    FirstCommit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSorts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 12, 34, 13, 45,67,78,90,1213,45,134};
+            int n=arr.Length;
+            int temp;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - 1 - i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+                Console.WriteLine("After sorting");
+                for (int k = 0; k < n; k++)
+                {
+                    Console.Write(arr[k] + " ");
+                }
+
+                Console.WriteLine();
+            Console.ReadKey();
+            
+        }
+    }
+}
